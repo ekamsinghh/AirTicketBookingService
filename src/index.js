2fd29ed15c12d45a64215dc8374a73b4c315cd07
Ekam Singh <ekamsinghh24@gmail.com>
2025-05-24 18:59:08 +0530

    Basic set up of the project

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const { PORT , DB_SYNC } = require("./config/server-config");
+const apiRoutes = require("./routes/index");
+const db=require("./models/index");
+const app = express();
+
+const startingAndSettingServer= ()=>{
+    
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    
+    app.use("/api",apiRoutes);
+
+    app.listen(PORT,()=>{
+        console.log("server started at",PORT);
+    });
+    if(DB_SYNC){
+        db.sequelize.sync({alter:true});
+    }
+
+}
+
+startingAndSettingServer();
